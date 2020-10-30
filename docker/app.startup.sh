83ef273e19318f4dd2b3c0a9fc8f9d1417bd013b
maspiter <maspiter@users.noreply.github.com>
2020-10-30 19:14:01 +0100

    Add files via upload

---
+++ docker/app.startup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+./manage.py makemigrations
+./manage.py migrate
+# in version 1.2 there were triggers defined
+# Since version 1.4, triggers are not used anymore
+# drop triggers
+./manage.py drop_triggers
+cat create_user.py | python3 manage.py shell
+
+./manage.py collectstatic --no-input
+./manage.py check
+
+# app worker process for file uploads
+nohup python manage.py worker &
+
+mod_wsgi-express start-server \
+    --server-root /opt/app/\
+    --url-alias /static /opt/static  \
+    --url-alias /media /opt/media \
+    --port 8000 --user www --group www \
+    --log-to-terminal \
+    --limit-request-body 20971520 \
+    config/wsgi.py
+
+# --limit-request-body 20971520 options is to limit
+# max upload size to 20 MB
---
+++ docker/worker.startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./manage.py makemigrations
+./manage.py migrate
+# in version 1.2 there were triggers defined
+# Since version 1.4, triggers are not used anymore
+# drop triggers
+./manage.py drop_triggers
+# Don't create admin user a second time
+#cat create_user.py | python3 manage.py shell
+./manage.py check
+python manage.py worker
